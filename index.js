66712718a4ddab6a5ee1c10c3cafb60fa25f47a5
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-13 23:50:43 +0100

    completed

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const mobileNavContainer = document.querySelector(".navigation-container-mobile"
 const structuralDiv = document.querySelector(".structural-div")
 
 function copyShortLink() {
-    //const id = this.getAttribute('data-argument')
-    //document.querySelector(`#${id}1`).value.select()
-    //document.execCommand("copy")
-    //console.log(document.querySelector(`#${id}1`).value)
-    //console.log(id)
-    console.log("registered")
+    const id = this.getAttribute('data-argument')
+    document.querySelector(`#o${id}`).select()
+    document.execCommand("copy")
 }
 
 const shortenLink = async (event) => {
@@ -19,28 +16,38 @@ const shortenLink = async (event) => {
         document.querySelector(".error-message").style.display = "block"
         document.querySelector(".link-input").style.border = "1px solid red"
     } else {
-        const longLink = document.querySelector(".link-input").value
-        const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`)
-        const res = await response.json()
-        const shortLink = res.result.short_link
-        console.log(shortLink)
+        try {
+            document.querySelector(".error-message").style.display = "none"
+            document.querySelector(".link-input").style.border = "none"
 
-        const longLinkToDisplay = document.querySelector(".link-input").value.length <= 23 ? document.querySelector(".link-input").value : `${document.querySelector(".link-input").value.substring(0, 22)}...`
+            const longLink = document.querySelector(".link-input").value
+            const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`)
+            const res = await response.json()
+            const shortLink = res.result.short_link
 
-        const linkDetails = `<div class="link-details-container">
-                                <p>${longLinkToDisplay}</p>
-                                <div>
-                                <input id=${shortLink + "1"} type="text" readonly value="${shortLink}" class="short-link">
-                                <button data-argument=${shortLink} id=${shortLink} class="copy-link-button">Copy</button>
-                                </div>
-                            </div>`
+            const longLinkToDisplay = document.querySelector(".link-input").value.length <= 23 ? document.querySelector(".link-input").value : `${document.querySelector(".link-input").value.substring(0, 22)}...`
 
-        document.querySelector(".second-div-container").innerHTML += linkDetails
-        document.querySelector(".short-link").style.width = document.querySelector(".short-link").value.length + "ch"
-        document.querySelector(".copy-link-button").addEventListener("click", () => console.log("registered"))
+            const linkDetails = `<div class="link-details-container">
+                                    <p>${longLinkToDisplay}</p>
+                                    <div>
+                                    <input id=${"o" + shortLink.substring(10, 14)} type="text" readonly value="${shortLink}" class="short-link">
+                                    <button data-argument=${shortLink.substring(10, 14)} id=${"p" + shortLink.substring(10, 14)} class="copy-link-button">Copy</button>
+                                    </div>
+                                </div>`
 
-        document.querySelector(".error-message").style.display = "none"
-        document.querySelector(".link-input").style.border = "none"
+            document.querySelector(".short-link-list-container").innerHTML += linkDetails
+            const shortLinkNodes = document.querySelectorAll(".short-link")
+            const copyLinkNodes = document.querySelectorAll(".copy-link-button")
+
+            for (let i = 0; i < shortLinkNodes.length; i++) {
+                shortLinkNodes[i].style.width = document.querySelector(".short-link").value.length + "ch"
+                copyLinkNodes[i].addEventListener("click", copyShortLink)
+            }
+
+            document.querySelector(".link-input").value = ""
+        } catch(err) {
+            console.log(err)
+        }
     }
 }
 
